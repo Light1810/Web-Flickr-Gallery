e04c63f8a00931c53396e1b83e7eac1dee4cdead
aakash_decosta <obito1810@gmail.com>
2020-04-07 22:47:42 +0530

    mionor changes

--- gallary.js
+++ gallary.js
@@ -1,16 +1,34 @@
 const btnSearch = document.getElementById("btnSearch");
 const divResult = document.getElementById("divResult");
+const searchBar = document.getElementById("searchBar");
+const webPageName =document.getElementById("WebPageName");
+const sticky = searchBar.offsetTop;
 btnSearch.addEventListener("click",getRepos);
+
+
+webPageName.addEventListener("click",()=>{
+    location.reload();
+});
+
+window.onscroll = () =>{
+
+    if (window.pageYOffset >= sticky) {
+        searchBar.classList.add("sticky")
+
+      } else {
+        searchBar.classList.remove("sticky");
+      }
+    
+}
 
 async function getRepos(){
 clear();
 const searchKey= document.getElementById("key").value;
-const baseUrl = "https://api.flickr.com/services/rest/?method=flickr.photos.search&per_page=24&page=1&api_key=6f102c62f41998d151e5a1b48713cf13&format=json&nojsoncallback=1&extras=url_s&tags="+ `${searchKey}`
+const baseUrl = "https://api.flickr.com/services/rest/?method=flickr.photos.search&per_page=25&page=1&api_key=6f102c62f41998d151e5a1b48713cf13&format=json&nojsoncallback=1&extras=url_s&tags="+ `${searchKey}`
 // params = {method:"flickr.photos.search" , per_page:10 , page:1 , api_key: "6f102c62f41998d151e5a1b48713cf13" , format: "json" , noJsonCallback :1 , extra : "url_s" , "text":searchKey}
 // Object.keys(params).forEach(key => baseUrl.searchParams.append(key, params[key]))
 
 // console.log(baseUrl);
-console.log(searchKey);
 
 
 try {
@@ -43,10 +61,12 @@ try {
 
 }
 
+  
+
 
 async function getDefault(){
     clear();
-    const baseUrl = "https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&per_page=20&page=1&api_key=6f102c62f41998d151e5a1b48713cf13&format=json&nojsoncallback=1&extras=url_s";
+    const baseUrl = "https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&per_page=25&page=1&api_key=6f102c62f41998d151e5a1b48713cf13&format=json&nojsoncallback=1&extras=url_s";
     try {
         const response =await fetch (baseUrl);
         try {
@@ -76,6 +96,7 @@ async function getDefault(){
       }
     
     }
+
 
 function clear(){
     while(divResult.firstChild)
